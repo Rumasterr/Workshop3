1885e9dd7c6383dfb627402600563c2803ff4f97
Rumasterr <145980671+Rumasterr@users.noreply.github.com>
2024-05-23 13:19:43 -0500

    Create structure.py

---
+++ structure.py
@@ -0,0 +1,55 @@
+#mostrar vehiculo
+[
+    {
+      "id": 1,
+      "make": "Toyota",
+      "model": "Corolla",
+      "year": 2020,
+      "engineId": 1
+    },
+    {
+      "id": 2,
+      "make": "Tesla",
+      "model": "Model S",
+      "year": 2021,
+      "engineId": 2
+    }
+  ]
+
+#show engine 
+{
+    "id": 1,
+    "name": "Motor V8",
+    "horsepower": 450,
+    "type": "Gasoline"
+  }
+
+#add vehicle
+{
+    "make": "Ford",
+    "model": "Mustang",
+    "year": 2022,
+    "engineId": 1
+  }
+
+{
+    "id": 3,
+    "make": "Ford",
+    "model": "Mustang",
+    "year": 2022,
+    "engineId": 1
+  }
+
+#add engine
+{
+    "name": "Motor Diesel",
+    "horsepower": 300,
+    "type": "Diesel"
+  }
+
+{
+    "id": 3,
+    "name": "Motor Diesel",
+    "horsepower": 300,
+    "type": "Diesel"
+   }
